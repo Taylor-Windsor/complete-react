a2f0518f70b17f864551b2c6d2522a3e59ed613a
Taylor Windsor <taylorjwindsor@gmail.com>
2023-03-03 17:03:10 -0500

    added props for expense component

--- getting-started/src/components/ExpenseItem.js
+++ getting-started/src/components/ExpenseItem.js
@@ -1,14 +1,19 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 2, 3);
-    const expenseTitle = 'Chick Fil A';
-    const expenseAmount = 25.00;
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('en-US', {dat: '2-digit'});
   return (
     <div className='expense-item'>
-    <div>{expenseDate.toISOString()}</div>
+        <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+        </div>
+    
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
