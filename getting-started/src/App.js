5d7ff2d159b6728a2f49386b13f58aab2f958c0e
Taylor Windsor <taylorjwindsor@gmail.com>
2023-03-07 22:52:19 -0500

    added expenseFilter and worked with sharing state between some of the expense componentes

--- getting-started/src/App.js
+++ getting-started/src/App.js
@@ -27,9 +27,13 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = expense => {
+    console.log(expense)
+  }
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
