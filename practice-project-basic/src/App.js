31089f06edc3cf71f4f43486f4dc26dddcf20a74
Taylor Windsor <taylorjwindsor@gmail.com>
2023-03-10 17:10:29 -0500

    added user list component driven by user input

--- practice-project-basic/src/App.js
+++ practice-project-basic/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AddUser from './components/Users/AddUser';
-
+import UsersList from './components/Users/Userslist'
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}]
+    })
+  }
   return (
     <div>
-      <AddUser/>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users ={usersList}/>
     </div>
   );
 }
